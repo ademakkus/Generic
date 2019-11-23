afffaca433eeef74cc8179e85f342b6b717f5ad3
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-11-23 16:58:44 +0200

    foreach added
    foreach added

--- ForEach/Program.cs
+++ ForEach/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace ForEach
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5,6,7,8,9,10 };
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("IEnumarator kullanımı");
+            IEnumerator numerator = numbers.GetEnumerator();
+            while (numerator.MoveNext())
+            {
+                Console.WriteLine(numerator.Current);
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
